016994dafdaaf2a2e3fba9078d66c6000387cced
ST10478055 <ST10478055@myemeris.edu.za>
2026-05-04 14:10:02 +0200

    Added Customer class with added business logic

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,4 +1,47 @@
 package org.example;
 
 public class Customer {
+
+    private final String name;
+    private int points;
+
+    // Constructor to initialize name and points
+
+    public Customer(String name) {
+        this.name = name;
+        this.points = 0;
+    }
+
+    // adds 10 points
+
+    public void addPoints() {
+        points += 10;
+        System.out.println("10 Points added");
+    }
+
+    // Redeems 50 points
+
+    public void redeemPoints() {
+        if (points >= 50) {
+            points -= 50;
+            System.out.println("Reward redeemed! Free coffee.");
+        } else {
+            System.out.println("Insufficient Points");
+        }
+    }
+
+    // user info
+
+    public void displayStatus() {
+        System.out.println("Member information:");
+        System.out.println("Name: " + name);
+        System.out.println("Points: " + points);
+    }
+
+    // tests
+
+
+    public int getPoints() {
+        return points;
+    }
 }
